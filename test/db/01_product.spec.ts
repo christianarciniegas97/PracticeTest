45e953cf6e2055c033d5d413e6ebbc51949e2f5c
chris barrios lider tecnico <christianeadres97@gmail.com>
2025-12-08 18:38:18 -0300

    nueva estructura

--- test/db/01_product.spec.ts
+++ test/db/01_product.spec.ts
@@ -1,5 +1,5 @@
-import { expect, test } from "../base";
-import { db } from "../utils/db";
+import { expect, test } from "../../fixture/base";
+import { db } from "../../utils/db";
 
 test('Validate all products', async () =>{
     const [products] = await db.query('SELECT * FROM productos')
